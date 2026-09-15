afdf66ccc0048c2789085370ef182c2d7e5b1282
NkAndre <andre270708@gmail.com>
2026-09-14 23:06:33 -0300

    projeto feito

---
+++ src/controleDeFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package controleDeFluxo;
+
+public class ParametrosInvalidosException  extends Exception{
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
